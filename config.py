8782d42dc79662cea06dc87128ba9abed37445e4
Alvyn Abranches <alvynabranches@gmail.com>
2022-06-12 19:41:30 +0530

    Updated config.py

--- config.py
+++ config.py
@@ -1,13 +1,12 @@
 import torch
 
-create_json = True
-
 # We're ready to define everything we need for training our Seq2Seq model
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # # To utilize only CPU even if GPU is available
-device = torch.device("cpu")
+# device = torch.device("cpu")
 
+create_json = True
 load_model = False
 save_model = True
 train_model = True
@@ -17,7 +16,7 @@ score_model = True
 num_epochs = 1
 start = 0
 learning_rate = 3e-4
-batch_size = 8
+batch_size = 16
 
 # File name for pth files.
 filename = "models/en_de_{}.pth"
